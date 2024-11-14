635705c465ce5b108f0e84af4cfad6e5d38b2bae
Dylan Champion <dylanchampion596@hotmail.com>
2024-11-14 12:38:55 +0000

    Reading database onto home page

--- JustAnotherTapasRestaurant/Pages/Index.cshtml.cs
+++ JustAnotherTapasRestaurant/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using JustAnotherTapasRestaurant.Data;
+using JustAnotherTapasRestaurant.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace JustAnotherTapasRestaurant.Pages
 {
@@ -7,14 +10,30 @@ namespace JustAnotherTapasRestaurant.Pages
 	{
 		private readonly ILogger<IndexModel> _logger;
 
-		public IndexModel(ILogger<IndexModel> logger)
-		{
-			_logger = logger;
-		}
 
-		public void OnGet()
-		{
 
-		}
-	}
+		private readonly JustAnotherTapasRestaurantContext _context;
+
+        public IndexModel(JustAnotherTapasRestaurantContext context)
+        {
+            _context = context;
+        }
+
+        public IList<MenuItem> MenuItem { get; set; } = default!;
+
+        [BindProperty]
+        public string Search { get; set; }
+
+        public void OnGet()
+        {
+            MenuItem = _context.MenuItems.FromSqlRaw("Select * FROM MenuItem").ToList();
+        }
+
+        public IActionResult OnPostSearch()
+        {
+            MenuItem = _context.MenuItems.FromSqlRaw
+                ("SELECT * FROM MenuItem WHERE item LIKE '" + Search + "%'").ToList();
+            return Page();
+        }
+    }
 }
